003701e1e6c3150b36bd36b958f2d890db3c69c8
Marco Scirea <marco.prolog@gmail.com>
2020-09-17 07:31:45 +0200

    Game from last year's package

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI; //add this when referencing UI elements
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public int score = 0;
+
+    private Rigidbody rb;
+
+    public Text scoreUI;
+    public Text winUI;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("got a point");
+        score++;
+        other.gameObject.SetActive(false);
+        scoreUI.text = "Score: " + score;
+
+        if (score == 7)
+        {
+            //activate the win text
+            winUI.enabled = true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Receiver.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Receiver : MonoBehaviour
+{
+    Transmitter transmitterComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject transmitterObject = GameObject.Find("Transmitter");
+        transmitterComponent = transmitterObject.GetComponent<Transmitter>();
+        Transform transmitterTransform = transmitterObject.GetComponent<Transform>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(transmitterComponent.score);
+    }
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
