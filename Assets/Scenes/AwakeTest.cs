ad306541abe2885e319fa62d42b99543ecd0477c
Marco Scirea <marco.prolog@gmail.com>
2020-09-17 10:06:45 +0200

    Made some examples during class, had to revert to basic input manager

---
+++ Assets/Scenes/AwakeTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeTest : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        Debug.Log("awake");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("update");
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log("fixedUpdate");
+    }
+}
---
+++ Assets/Scenes/FindTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindTest : MonoBehaviour
+{
+    public GameObject mainCameraObject;
+    public Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCameraObject = GameObject.Find("Main Camera");
+        camera = mainCameraObject.GetComponent<Camera>();
+        // gameObject.GetComponent<Camera>(); == GetComponent<Camera>();
+        //  gameObject == reference to the game object that the script is currently on
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
